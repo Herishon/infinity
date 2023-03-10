2a41853feb361aa8aadc0aa9187922da331eb934
Herishon Dalbot <74166563+Herishon@users.noreply.github.com>
2023-03-10 16:09:07 +0600

    Updated

--- js/app.js
+++ js/app.js
@@ -29,22 +29,6 @@ noteTogglerBtn.addEventListener("click", function () {
         noteTogglerIcon.classList.add("fa-plus");
         noteTogglerIcon.classList.remove("fa-minus");
     }
-
-})
-
-
-// Top head section BDT-USD drop-down function
-const currencyBtn = document.getElementById("drop_btn");
-const dropMenu = document.getElementById("drop_menu");
-
-currencyBtn.addEventListener("click", function () {
-
-    if(dropMenu.style.display == "none") {
-        dropMenu.style.display = "block";
-    } else {
-        dropMenu.style.display = "none";
-    }
-
 })
 
 
@@ -77,115 +61,13 @@ $('#mobile-menu-btn').click(function () {
 
 
 // Collapsible items
-const collBtn1 = document.getElementById("collBtn1");
-const collItem1 = document.getElementById("collItem1");
-const icon1 = document.querySelector(".arrow1");
+let collapseBtn = document.querySelectorAll('.coll-btn');
+let len = document.querySelectorAll('.coll-btn').length;
+for(let i = 0; i < len; i++) {
+    collapseBtn[i].addEventListener('click', function(){
 
-collBtn1.addEventListener("click", function () {
-
-    let letDecide = "";
-
-    if(collItem1.style.display == "block") {
-        collItem1.style.display = "none";
-        letDecide = "none";
+        collapseBtn[i].nextElementSibling.classList.toggle('show-coll');
+        collapseBtn[i].querySelector('.fa-angle-down').classList.toggle("fa-angle-up");
         
-    } else {
-        collItem1.style.display = "block";
-        letDecide = "block";
-    }
-
-    if(letDecide == "block") {
-        console.log("angle-up");
-        icon1.classList.remove("fa-angle-down");
-        icon1.classList.add("fa-angle-up");
-    } else {
-        console.log("angle-down");
-        icon1.classList.remove("fa-angle-up");
-        icon1.classList.add("fa-angle-down");
-    }
-
-})
-
-const collBtn2 = document.getElementById("collBtn2");
-const collItem2 = document.getElementById("collItem2");
-const icon2 = document.querySelector(".arrow2");
-
-collBtn2.addEventListener("click", function () {
-
-    let letDecide = "";
-
-    if(collItem2.style.display == "block") {
-        collItem2.style.display = "none";
-        letDecide = "none";
-    } else {
-        collItem2.style.display = "block";
-        letDecide = "block";
-    }
-
-    if(letDecide == "block") {
-        console.log("angle-up");
-        icon2.classList.remove("fa-angle-down");
-        icon2.classList.add("fa-angle-up");
-    } else {
-        console.log("angle-down");
-        icon2.classList.remove("fa-angle-up");
-        icon2.classList.add("fa-angle-down");
-    }
-
-})
-
-const collBtn3 = document.getElementById("collBtn3");
-const collItem3 = document.getElementById("collItem3");
-const icon3 = document.querySelector(".arrow3");
-
-collBtn3.addEventListener("click", function () {
-
-    let letDecide = "";
-
-    if(collItem3.style.display == "block") {
-        collItem3.style.display = "none";
-        letDecide = "none";
-    } else {
-        collItem3.style.display = "block";
-        letDecide = "block";
-    }
-
-    if(letDecide == "block") {
-        console.log("angle-up");
-        icon3.classList.remove("fa-angle-down");
-        icon3.classList.add("fa-angle-up");
-    } else {
-        console.log("angle-down");
-        icon3.classList.remove("fa-angle-up");
-        icon3.classList.add("fa-angle-down");
-    }
-
-})
-
-const collBtn4 = document.getElementById("collBtn4");
-const collItem4 = document.getElementById("collItem4");
-const icon4 = document.querySelector(".arrow4");
-
-collBtn4.addEventListener("click", function () {
-
-    let letDecide = "";
-
-    if(collItem4.style.display == "block") {
-        collItem4.style.display = "none";
-        letDecide = "none";
-    } else {
-        collItem4.style.display = "block";
-        letDecide = "block";
-    }
-
-    if(letDecide == "block") {
-        console.log("angle-up");
-        icon4.classList.remove("fa-angle-down");
-        icon4.classList.add("fa-angle-up");
-    } else {
-        console.log("angle-down");
-        icon4.classList.remove("fa-angle-up");
-        icon4.classList.add("fa-angle-down");
-    }
-    
-})
+    })
+}
